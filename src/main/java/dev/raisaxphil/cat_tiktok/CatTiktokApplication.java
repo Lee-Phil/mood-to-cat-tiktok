aab0f8443b751e23429469dc455da2e404539c5d
Philippe Lee <philippelee@philippes-mbp.home>
2024-06-28 20:09:35 -0400

    Initial Commit

---
+++ src/main/java/dev/raisaxphil/cat_tiktok/CatTiktokApplication.java
@@ -0,0 +1,13 @@
+package dev.raisaxphil.cat_tiktok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatTiktokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatTiktokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/raisaxphil/cat_tiktok/CatTiktokApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.raisaxphil.cat_tiktok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatTiktokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
